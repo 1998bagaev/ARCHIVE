192679ca13cd5bfcfcd07e142569cebcf8f82c3f
1998bagaev <viktor.bagaev.98@mail.ru>
2016-06-15 23:43:50 +0300

    Create lab6_arch.h

---
+++ lab6_arch.h
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+
+class vector {
+public:
+	vector(float x = 0, float y = 0)
+		:_x(x), _y(y) {}
+
+	void coord(float x, float y) {
+		_x = x;
+		_y = y;
+	}
+
+	std::pair<float, float> getPoint() {
+		return{ _x, _y };
+	}
+
+	float length() {
+		return sqrt(pow(_x, 2) + pow(_y, 2));
+	}
+
+	void printcoord() {
+		std::cout << "Point: { " << _x << ", " << _y << " }" << std::endl;
+	}
+
+	void printLength() {
+		std::cout << "Length of vector: " << length() << std::endl;
+	}
+
+	~vector() = default;
+
+protected:
+	float _x;
+	float _y;
+};
+
+class vectorE : public vector {
+public:
+	vectorE(float x = 0, float y = 0, float z = 0)
+		:vector(x, y), _z(z){}
+
+	void coord(float x, float y, float z) {
+		_x = x;
+		_y = y;
+		_z = z;
+	}
+
+	float length() {
+		return sqrt(pow(_x, 2) + pow(_y, 2) + pow(_z, 2));
+	}
+
+	void printcoord() {
+		std::cout << "Point: { " << _x << ", " << _y << ", " << _z << " }" << std::endl;
+	}
+
+	void printLength() {
+		std::cout << "Length of vector: " << length() << std::endl;
+	}
+
+	~vectorE() = default;
+protected:
+	float _z;
+};
